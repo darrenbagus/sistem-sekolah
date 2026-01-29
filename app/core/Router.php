957a8ae1392dc26d12b4c00a599b6a6c6ffcd38d
darrenbagus <darrenbagus1140@gmail.com>
2026-01-29 13:04:05 +0700

    feat: implement simple router

--- app/core/Router.php
+++ app/core/Router.php
@@ -2,13 +2,40 @@
 
 namespace App\Core;
 
+use App\Controllers\StudentController;
+
 class Router
 {
 
-public function run ()
-{
-    // Routing logic goes her
-
+  public function run ()
+    {
+       $method = $_SERVER['REQUEST_METHOD'];
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+ 
+   if ($method == 'GET' && $uri == '/students') {
+    require_once './app/controllers/StudentController.php';
+    $controller = new studentController();
+    $controller->index();
+    return;
+ 
+   }
+ 
+    $method = $_SERVER['REQUEST_METHOD'];
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+ 
+   if ($method == 'GET' && $uri == '/students/create') {
+      require_once './app/controllers/StudentController.php';
+    $controller = new studentController();
+    $controller->create(); 
+    return;
+ 
+   }
+ 
+   http_response_code(404);
+   echo '<h1>404 - Page Not Found</h1>';
 }
+    
+
+   
 
 }
